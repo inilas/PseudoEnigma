23bb59367e46bedb5a6d2cd77ab0838d9d311274
PNadal260 <pau.nadal@estudiant.enti.cat>
2026-01-15 10:59:48 +0100

    Eliminar caràcteres especiales( sin terminar)

---
+++ PseudoEnigma/getMessage.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+std::string getMessage() {
+std::string messageToCypher;
+//Eliminar caracteres especiales
+std::string specialChars = ",.;:-!\"!?¡¿";
+std::cout << "Introduce un mensaje para cifrar.";
+std::getline(std::cin, messageToCypher);
+for (int i = 0; i < messageToCypher.length(); i++)
+{
+	
+}
+
+}
